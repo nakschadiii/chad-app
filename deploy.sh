6d1a9af41d23be1ddfed49b3e85060571dd0fe59
MTOKO <mbala.toko@ynov.com>
2023-05-03 00:58:52 +0200

    ...

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 pm2 stop app
-mysqldump -u root -p --databases cosmo > return.sql
+mysqldump -u root --databases cosmo > return.sql
 git pull https://github.com/nakschadiii/chad-app.git master
-mysql -u root -p cosmo < users.sql
+mysql -u root cosmo < users.sql
 pm2 start app
