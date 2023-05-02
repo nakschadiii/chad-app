f95aee290a1a51e0bc8dd12719f04e38d9cbaa48
MTOKO <mbala.toko@ynov.com>
2023-05-02 20:50:39 +0200

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,9 @@ import NotLoggedPage from './components/notLoggedPage';
 import HomePage from './components/HomePage';
 import Logout from './components/Logout';
 
-//const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL || 'http://localhost:4000';
-//const socket = socketIO.connect(SOCKET_SERVER_URL);
-const socket = socketIO.connect('http://127.0.0.1:4000');
+const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL || 'http://localhost:4000';
+const socket = socketIO.connect(SOCKET_SERVER_URL);
+//const socket = socketIO.connect('http://127.0.0.1:4000');
 
 function App() {
   const notLogged = Cookies.get('8bb052b406ffac0b420918a77c905b0c_user') === undefined | null;
