a81fb2dfd35f201826e8252f8e7be278ff546b57
MTOKO <mbala.toko@ynov.com>
2023-05-03 01:00:55 +0200

    ...

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 pm2 stop app
 mysqldump -u root --databases cosmo > return.sql
+git add return.sql && git commit -m "rendu base de données online > local" && git push
 git pull https://github.com/nakschadiii/chad-app.git master
 mysql -u root cosmo < users.sql
 pm2 start app
