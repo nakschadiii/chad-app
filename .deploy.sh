caf3e964fabcf81339a26f3f28343339b8d6ece9
MTOKO <mbala.toko@ynov.com>
2023-05-03 09:48:04 +0200

    ...

--- .deploy.sh
+++ .deploy.sh
@@ -1,6 +1,7 @@
 clear
 cd /var/www/85.90.245.34/
 mysqldump -u root --databases cosmo > return.sql
+git commit -m '...'
 git pull https://github.com/nakschadiii/chad-app.git master
 mysql -u root cosmo < users.sql
 pm2 restart app
